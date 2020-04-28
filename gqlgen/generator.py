003748f71f9dbe4939eb455174b235a424f1c063
syfun <sunyu418@gmail.com>
2020-04-28 12:11:46 +0800

    to 0.0.1

--- gqlgen/generator.py
+++ gqlgen/generator.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from typing import Dict, List, Union, cast
 
+from gql.utils import to_snake_case
 from graphql import (
     GraphQLEnumType,
     GraphQLField,
@@ -12,7 +13,6 @@ from graphql import (
     GraphQLType,
     GraphQLWrappingType,
     Source,
-    TypeDefinitionNode,
     assert_object_type,
     assert_union_type,
     get_named_type,
@@ -23,13 +23,10 @@ from graphql import (
     is_object_type,
     is_union_type,
     is_wrapping_type,
-    parse,
 )
 from graphql.pyutils.cached_property import cached_property
-# from graphql.utilities.build_ast_schema import ASTDefinitionBuilder
-from graphql.validation.validate import assert_valid_sdl
 
-from gql.utils import to_snake_case
+# from graphql.utilities.build_ast_schema import ASTDefinitionBuilder
 
 TypeMap = Dict[str, GraphQLNamedType]
 SourceType = Union[Source, str]
